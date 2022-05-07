61d3b546b7993034b4951a9880e400d80d073c75
Ryuk <51106560+faroukbmiled@users.noreply.github.com>
2022-05-07 05:37:59 +0100

    v1

---
+++ Workshop Downloader.ps1
@@ -0,0 +1,21 @@
+﻿$Url = "http://workshop2.abcvg.info/archive/431240/1415958558.zip"
+
+$cpitem = "C:\Program Files (x86)\Steam\steamapps\workshop\content\480\1415958558"
+
+$default = "C:\Program Files (x86)\Steam\steamapps\workshop\content\480\2335511276"
+
+$DownloadZipFile = "C:\Program Files (x86)\Steam\steamapps\workshop\content\" + $(Split-Path -Path $Url -Leaf)
+
+$ExtractPath = "C:\Program Files (x86)\Steam\steamapps\workshop\content\480"
+
+Invoke-WebRequest -Uri $Url -OutFile $DownloadZipFile
+
+$ExtractShell = New-Object -ComObject Shell.Application 
+
+$ExtractFiles = $ExtractShell.Namespace($DownloadZipFile).Items() 
+
+$ExtractShell.NameSpace($ExtractPath).CopyHere($ExtractFiles)
+
+Get-ChildItem -Path $cpitem -Recurse -Force |
+  Where-Object {$_.LastWriteTime -lt (Get-date).AddDays(-31)} |
+  Move-Item -destination $default -Force
