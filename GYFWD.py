89225816e2dea9a648660a8a546aead6fd06ea4c
faroukbmiled <fcfaroukfc@gmail.com>
2023-04-30 13:30:45 +0100

    some improvments

--- GYFWD.py
+++ GYFWD.py
@@ -6,9 +6,21 @@ import json
 import webbrowser
 import time
 
-print('\n*********************************')
-print('* Ryuk GWYF Workshop downloader *')
-print('*********************************')
+ryuk_art = '''
+ _______  __      __  __    __  __    __
+|       \|  \    /  \|  \  |  \|  \  /  \\
+| $$$$$$$\\$$\\  /  $$| $$  | $$| $$ /  $$
+| $$__| $$ \$$\\/  $$ | $$  | $$| $$/  $$
+| $$    $$  \\$$  $$  | $$  | $$| $$  $$
+| $$$$$$$\\   \\$$$$   | $$  | $$| $$$$$\\
+| $$  | $$   | $$    | $$__/ $$| $$ \$$\\
+| $$  | $$   | $$     \\$$    $$| $$  \\$$\\
+ \\$$   \\$$    \\$$      \\$$$$$$  \\$$   \\$$
+'''
+print('\n****************************************')
+print(ryuk_art)
+print('*     Ryuk GWYF Workshop downloader     *')
+print('*****************************************')
 
 try:
 
@@ -18,24 +30,25 @@ try:
     else:
         saved_link = ''
 
-    print("\nChecking for updates...")
+    print("\n[*] Checking for updates...")
     response = requests.get('https://raw.githubusercontent.com/faroukbmiled/GWYFWD/main/vars.txt')
     current_link = response.text.strip().split()[0]
     id = response.text.strip().split()[0]
 
     if current_link != saved_link:
-        print("\nAn update found.\n")
+        print("\n[+] An update found.\n")
         with open('workshop.json', 'w') as f:
             json.dump(current_link, f)
     else:
-        print("\nNo updates found...\n")
+        print("\n[-] No updates found...\n")
         id = input("Enter Workshop id (enter to downlaod same map):") or saved_link
         print("\n")
 
     if not os.path.exists('settings.ini'):
-        print("Creating settings.ini...")
+        print("[+] Creating settings.ini...")
         while True:
-            installation_dir = input('Enter the Steam directory (Default: "C:\\Program Files (x86)"), or type "Fedi" for "C:\\Program Files": \n')
+            installation_dir = input('[!] Enter the Steam directory (Default: "C:\\Program Files (x86)"), or type "Fedi" for "C:\\Program Files": ')
+            print('\n')
             if installation_dir == '' or installation_dir.lower() == 'default':
                 installation_dir = 'C:\\Program Files (x86)'
                 break
@@ -43,7 +56,7 @@ try:
                 installation_dir = 'C:\\Program Files'
                 break
             else:
-                print('Invalid input. Please enter a valid Steam directory "Default" or type "Fedi" for "C:\\Program Files".\n')
+                print('[?] Invalid input. Please enter a valid Steam directory "Default" or type "Fedi" for "C:\\Program Files".\n')
 
         with open('settings.ini', 'w') as f:
             f.write(installation_dir)
@@ -55,9 +68,9 @@ try:
 
     if not os.path.exists(defaultPath):
         os.makedirs(defaultPath)
-        print("Workshop folder was not found but was created")
-        print("You need to subscribe to this map for this to work")
-        print("https://steamcommunity.com/sharedfiles/filedetails/?id=2335511276, link is opening in 3 seconds...\n")
+        print("[-] Workshop folder was not found but was created")
+        print("[!] You need to subscribe to this map for this to work")
+        print("[!] https://steamcommunity.com/sharedfiles/filedetails/?id=2335511276, link is opening in 3 seconds...\n")
         time.sleep(3)
         webbrowser.open("https://steamcommunity.com/sharedfiles/filedetails/?id=2335511276")
 
@@ -68,7 +81,7 @@ try:
             if response.status_code == 200:
                 break
     except:
-        print("Workshop Server error, try again or try another map")
+        print("[NetErr] Workshop Server error, try again or try another map")
         exit()
 
     cpitem = os.path.join(os.environ['USERPROFILE'], 'AppData', 'Local', 'Temp', id)
@@ -77,40 +90,40 @@ try:
 
     shutil.rmtree(defaultPath, ignore_errors=True)
 
-    print("Downloading Workshop map...\n")
+    print("[*] Downloading Workshop map...\n")
 
     try:
         response = requests.get(url, stream=True)
         with open(downloadZipFile, 'wb') as f:
             shutil.copyfileobj(response.raw, f)
     except:
-        print("Failed to download ,check internet connection or contact Ryuk\n")
+        print("[Err] Failed to download ,check internet connection or contact Ryuk\n")
         exit()
 
-    print("Extracting Workshop map...\n")
+    print("[+] Extracting Workshop map...\n")
     try:
         with zipfile.ZipFile(downloadZipFile, 'r') as zip_ref:
             zip_ref.extractall(extractPath)
     except:
-        print('Failed extracting fildes to /tmp/, retry or contact Ryuk\n')
+        print('[Err] Failed extracting fildes to /tmp/, retry or contact Ryuk\n')
 
-    print("Copying Workshop map to steam folder...\n")
+    print("[+] Copying Workshop map to steam folder...\n")
     try:
         shutil.copytree(cpitem, defaultPath, dirs_exist_ok=True)
     except:
-        print("Failed to copy ,Retry or contact Ryuk\n")
+        print("[Err] Failed to copy ,Retry or contact Ryuk\n")
         exit()
 
-    print("Removing Downloaded zip from /tmp/ and leaving...\n")
+    print("[-] Removing Downloaded zip from /tmp/ and leaving...")
 
     try:
         shutil.rmtree(cpitem, ignore_errors=True)
+        os.remove(downloadZipFile)
     except:
-        print("Failed to delete junk, script is passing anyway\n")
+        print("\n[Err] Failed to delete junk, script is passing anyway\n")
         pass
-
-    os.remove(downloadZipFile)
+    print('\n[✓] Done!')
 
 except:
-    print("Unexpexted error occured ,contact Ryuk to debug")
+    print("[Err] Unexpexted error occured ,contact Ryuk to debug")
     exit()
